3a73e045bae45ab09fd876e78328c530eeec2522
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-06-22 00:37:13 -0700

    added first program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int a,b;
+        System.out.println("Enter the 1st number:");
+        a=sc.nextInt();
+        System.out.println("Enter the 2nd number:");
+        b=sc.nextInt();
+        System.out.println("The sum of two number is:"+(a+b));
+
+
+        sc.close();
+        
+    }
+}
