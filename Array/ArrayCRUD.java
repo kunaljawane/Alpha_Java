4156292029fba7f630cc5c0cb978e1e2f0a28cab
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-07-12 04:11:53 -0700

    array

---
+++ Array/ArrayCRUD.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class ArrayCRUD {
+
+    public static void main(String args[]){
+        // int marks[]=new int[5];
+
+        // for(int i=0;i<5;i++){
+        //     marks[i]=i+1;
+        // }
+
+        // for(int i=0;i<=5;i++)
+        // {
+        //     System.out.println(marks[i]);
+        // }
+
+        String names[]={"kunal","jawane"};
+
+        System.out.println(names[1]);
+
+        
+
+
+    }
+    
+}
---
+++ Array/Into.java
@@ -0,0 +1,39 @@
+package Array;
+
+public class Into {
+
+    //arrays is the data structure like list where element stored in the contagious memory block
+    //had index start from 0
+    //list of element of same type
+
+    //size of int-4 Bytes
+    //size of char-1 Byte
+
+    //creation:
+    public static void main(String args[]){
+        int marks[]=new int[5];
+
+        int marks2[]={1,2,3};
+
+        char marks3[]={'a','b','c'};
+
+        System.out.println(marks[1]);
+
+        System.out.println(marks2[2]);
+
+        System.out.println(marks3[2]);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    
+}
