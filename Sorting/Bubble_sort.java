dc4e1d2e3532cd0fb674d35896ca9964c57606c7
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-07-13 23:29:14 -0700

    sort

---
+++ Sorting/Bubble_sort.java
@@ -0,0 +1,45 @@
+package Sorting;
+
+public class Bubble_sort {
+
+    public static void Bubble(int arr[]){
+
+        for(int i=0;i<arr.length-1;i++) 
+        {
+            for(int j=0;j<arr.length-1-i;j++)
+            {
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+    }
+
+    public static void print(int arr[]){
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+        int arr[]={5,4,3,2,1};
+
+        Bubble(arr);
+
+        print(arr);
+    }
+    
+}
+
+// bubble sort is the sorting technique in which the element is exploerd one by one
+// and greater element is pushed to the end
+// firstly the element is compared with the next element and then pushed forward
+// Time complexity=O(n square)
+
---
+++ Sorting/Selection_sort.java
@@ -0,0 +1,47 @@
+package Sorting;
+
+public class Selection_sort {
+    public static void selection(int arr[]){
+
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int min=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[min]>arr[j])
+                {
+                    min=j;
+                }
+
+            }
+
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+
+    }
+
+    public static void print(int arr[]){
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+        int arr[]={12,14,11,15};
+
+        selection(arr);
+
+        print(arr);
+    }
+}
+
+// in the slection sort we use idea opposite to bubble sort insted of swapping one by one
+// we directly find for the smallest element and bring that element at the start and one 
+// by one we get the sorted array
+// time complextiy is O(n square)
