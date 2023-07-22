b406d6bb00e3d59cbfa661df447d442eee3bd45d
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-07-21 23:09:21 -0700

    oops

---
+++ Oops/classes.java
@@ -0,0 +1,13 @@
+public class classes{
+    public static void main(String args[])
+    {
+        student s=new student();
+        s.name="kunal";
+
+    }
+}
+
+class student{
+    int no;
+    String name;
+}
