d30aa692ceacbd21b0184a1fd5772f2cbd8b1ec2
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-08-20 10:26:02 +0530

    Revision

--- Problems/NextGreater.java
+++ Problems/NextGreater.java
@@ -1,5 +1,51 @@
+package Problems;
 public class NextGreater {
+
+    public static void nextG(int[] arr)
+    {
+        int[] res=new int[arr.length];
+
+        int idx=0;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j]>arr[i])
+                {
+                    res[idx]=arr[j];
+                    idx++;
+                    break;
+
+                }
+            }
+            if(res[i]==0)
+            {
+                res[i]=-1;
+               
+                
+            }
+
+        }
+
+
+
+
+
+        for(int k=0;k<res.length;k++)
+        {
+            System.out.print(res[k]+" ");
+        }
+
+    }
     
+    public static void main(String args[])
+    {
+        int[] find={10,4,1,6,2,1,3,7,2,5,9,1};
+
+        nextG(find);
+
+    }
 }
 
 
